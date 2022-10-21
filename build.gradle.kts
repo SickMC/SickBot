19c5a37746c362e77736fbc00a1d30633b6f887f
Anton <anton.wiblishauser@gmx.de>
2022-10-21 20:33:50 +0200

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,29 @@
 plugins {
     application
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
 }
 
 group = "net.sickmc"
-version = "1.0"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
-
     maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
-    implementation("dev.kord:kord-core:0.8.x-SNAPSHOT")
-    implementation("dev.kord.x:emoji:0.5.0")
+    implementation("dev.kord:kord-core:0.8.0-M16")
 
-    implementation("org.litote.kmongo:kmongo-coroutine:4.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.litote.kmongo:kmongo-coroutine-serialization:4.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
-    implementation("org.slf4j:slf4j-simple:2.0.0-alpha7")
+    implementation("net.sickmc:sickapi:1.0.8")
 
-    val ktorVersion = "2.0.3"
+    implementation("org.slf4j:slf4j-simple:2.0.3")
+
+    val ktorVersion = "2.1.2"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-websockets:$ktorVersion")
@@ -31,15 +34,13 @@ kotlin.sourceSets.all {
 }
 
 tasks{
-
     compileJava{
         options.encoding = "UTF-8"
-        options.release.set(17)
+        options.release.set(18)
     }
     compileKotlin{
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "18"
     }
-
 }
 
 application{
