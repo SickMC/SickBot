405c7ae8173aa3b529bba2c9e38103cdb0afb1dc
Anton <anton.wiblishauser@gmx.de>
2022-11-15 16:29:59 +0100

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
 
     id("com.github.breadmoirai.github-release") version "2.4.1"
 }
@@ -11,21 +11,18 @@ version = "2.0.0"
 
 repositories {
     mavenCentral()
-    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
-    implementation("dev.kord:kord-core:0.8.0-M16")
+    implementation("dev.kord:kord-core:0.8.0-M17")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.litote.kmongo:kmongo-coroutine-serialization:4.7.1")
+    implementation("org.litote.kmongo:kmongo-coroutine-serialization:4.7.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
-    implementation("net.sickmc:sickapi:1.0.10")
+    implementation("net.sickmc:sickapi:1.0.12")
 
-    implementation("org.slf4j:slf4j-simple:2.0.3")
-
-    val ktorVersion = "2.1.2"
+    val ktorVersion = "2.1.3"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-websockets:$ktorVersion")
@@ -33,16 +30,6 @@ dependencies {
 
 kotlin.sourceSets.all {
     languageSettings.optIn("kotlin.RequiresOptIn")
-}
-
-tasks{
-    compileJava{
-        options.encoding = "UTF-8"
-        options.release.set(18)
-    }
-    compileKotlin{
-        kotlinOptions.jvmTarget = "18"
-    }
 }
 
 application{
